00e457fe57e86a82ee31401787612241082541e1
alisiben <alisiben@outlook.com>
2018-03-25 12:25:10 +0800

    initial RazorPagesMovie

---
+++ RazorPagesMovie/RazorPagesMovie/Models/FileUpload.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class FileUpload
+    {
+        [StringLength(60, MinimumLength = 3), Display(Name = "Title"), Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Public Schedule"), Required]
+        public IFormFile UploadPublicSchedule { get; set; }
+
+        [Display(Name = "Private Schedule"), Required]
+        public IFormFile UploadPrivateSchedule { get; set; }
+    }
+}
