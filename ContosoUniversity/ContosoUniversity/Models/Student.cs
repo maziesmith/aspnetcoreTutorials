f925022dab4432c7ce7658f219fc25f35c27122c
alisiben <alisiben@outlook.com>
2018-03-26 12:45:56 +0800

    Inheritance

--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -5,29 +5,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-
-        [Display(Name = "Last Name"), StringLength(50), Required]
-        public string LastName { get; set; }
-
-        [Display(Name = "First Name"), Column("FirstName"), Required]
-        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
-        public string FirstMidName { get; set; }
-
-        [Display(Name = "Enrollment Date"), DataType(DataType.Date)]
+        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
 
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
-        }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
